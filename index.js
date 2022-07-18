8ce491e367683ca75827279f1078599932dd9f61
Mitali2700 <201B155@juetguna.in>
2022-07-18 16:47:04 +0530

    sixth commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+//  <h1 align="center">jQuery</h1>
+
+
 //$("h1").addClass("big-title");
-$("h1").text("Bye");
+//$("h1").text("Bye");
 // Minifier.org - minify the JS and CSS code .
 
 //$("button").text("Don't Click Me");
@@ -8,3 +11,11 @@ $("button").html("<em>Hey</em>")
 //$("button").text("<em>Hey< /em>")      //Button O/P <em>Hey</em>
 
 $("a").attr("href","https:/www.yahoo.com"); 
+
+
+/************** Adding Eventlistener with jQuery *************/
+
+$("h1").click(function(){
+    $("h1").css("color","pink")
+});
+
