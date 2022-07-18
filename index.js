7ef4926adada173b4c0b7155e989fe0e1b35cb15
Mitali2700 <201B155@juetguna.in>
2022-07-18 17:00:16 +0530

    seventh commit

--- index.js
+++ index.js
@@ -15,7 +15,26 @@ $("a").attr("href","https:/www.yahoo.com");
 
 /************** Adding Eventlistener with jQuery *************/
 
-$("h1").click(function(){
-    $("h1").css("color","pink")
+// $("h1").click(function(){
+//     $("h1").css("color","pink")
+// });
+
+/************** Adding Eventlistener with jQuery- for loop *************/
+
+/*     JavaScript
+for(var i=0; i<5; i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        document.querySelector("h1").style.color ="purple";
+    });
+}
+*/
+
+// jQuery
+
+$("button").click(function(){
+    $("h1").css("color","purple");
 });
 
+$("input").keypress(function(event){
+    console.log(event.key);
+});
